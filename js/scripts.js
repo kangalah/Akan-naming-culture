67da0fe1c7bd0d45b816fc34efb051c1536e52d5
joan <nyongesajoannanjala@gmail.com>
2020-04-30 21:15:04 +0300

    created an array to hold the akan male names

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,2 @@
-var dayOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 var daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var akanMaleNames =['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame',];
