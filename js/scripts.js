8ed80be0581ab46ae2afc3d2f3e926bb824b97ae
joan <nyongesajoannanjala@gmail.com>
2020-04-30 20:59:52 +0300

    created an array to hold days of the week

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,2 @@
 var dayOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-var maleName = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
-alert(akanName);
+var daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
