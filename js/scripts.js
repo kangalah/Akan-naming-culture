74e438ac80faf57682768f12e06087bc6cf2d04f
joan <nyongesajoannanjala@gmail.com>
2020-04-30 22:48:04 +0300

    added a variable to store the year and get the akan name

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,5 @@ var akanFemaleNames=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama',];
 function getName (){
     var day = parseInt(document.getElementById('day').value);
     var month = parseInt(document.getElementById('month').value);
-    
+    var year = parseInt(document.getElementById('year').value);
 
