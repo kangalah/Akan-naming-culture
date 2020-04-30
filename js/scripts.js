8419e022447b92c68d1fdaf49717eda2dceb071f
joan <nyongesajoannanjala@gmail.com>
2020-04-30 22:42:02 +0300

    created a function to get the name when a day entered is valid

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
 var daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 var akanMaleNames =['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame',];
 var akanFemaleNames=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama',];
+
+function getName (){
+    var day = parseInt(document.getElementByIde("day").value);
