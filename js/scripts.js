951252d098fc0f4b283289f2f4ec7fa2eeb49c9f
joan <nyongesajoannanjala@gmail.com>
2020-04-30 22:33:24 +0300

    created an array to store the akan female names

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,3 @@
 var daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 var akanMaleNames =['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame',];
+var akanFemaleNames=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama',];
