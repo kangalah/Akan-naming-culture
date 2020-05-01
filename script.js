c59b2506aaf27ed495fec55eb32aeb69a5c1f491
joan <nyongesajoannanjala@gmail.com>
2020-05-01 15:48:17 +0300

    modified html and js file

--- script.js
+++ script.js
@@ -8,21 +8,21 @@ var akanFemaleNames=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama',];
     var year = parseInt(document.getElementById('year').value);
     var male = document.getElementById('male').value);
     var female = document.getElementById('female').value);
-
+      
     if (day<=0||day>31)
     alert('Please enter a valid day');
     else 
     if (month<0||month>12 ||month ==2 && day>29)
     alert('Please enter a valid month');
 
-    var day = new Date(year + '/' + month + '/' + day);
+    var day = new Date(day + '/' + month + '/' + year);
     var birthDay = day.getDay();
     if (male.checked==true){
     alert("You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + akanMaleNames[birthDay])
-    }
+    
 
     else 
     if (female.checked==true){
         alert("You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + akanFemaleNames[birthDay])
-     }
+    }
      }
